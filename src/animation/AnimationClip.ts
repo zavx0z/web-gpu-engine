908bbe9794e5d525791f43ab60c65f71598b5de5
zavx0z <zavx0z@yahoo.com>
2026-01-28 00:02:37 +0300

    [refactor] animation - форматирование кода и удаление неиспользуемого импорта
    ### Улучшения кода:
    - Изменены импорты с `;` на новые строки и исправлены кавычки в `AnimationAction.ts`
    - Удален неиспользуемый импорт `SkinnedMesh` из `AnimationMixer.ts`
    - Приведен в порядок формат файлов (убраны точки с запятой, исправлены отступы)

--- src/animation/AnimationClip.ts
+++ src/animation/AnimationClip.ts
@@ -1,24 +1,24 @@
-import { KeyframeTrack } from "./KeyframeTrack";
+import { KeyframeTrack } from "./KeyframeTrack"
 
 export class AnimationClip {
-  public name: string;
-  public tracks: KeyframeTrack[];
-  public duration: number;
+  public name: string
+  public tracks: KeyframeTrack[]
+  public duration: number
 
   constructor(name: string, duration: number = -1, tracks: KeyframeTrack[] = []) {
-    this.name = name;
-    this.tracks = tracks;
-    this.duration = (duration >= 0) ? duration : this.calculateDuration();
+    this.name = name
+    this.tracks = tracks
+    this.duration = duration >= 0 ? duration : this.calculateDuration()
   }
 
   private calculateDuration(): number {
-    let maxDuration = 0;
+    let maxDuration = 0
     for (const track of this.tracks) {
-      const trackDuration = track.times[track.times.length - 1] || 0;
+      const trackDuration = track.times[track.times.length - 1] || 0
       if (trackDuration > maxDuration) {
-        maxDuration = trackDuration;
+        maxDuration = trackDuration
       }
     }
-    return maxDuration;
+    return maxDuration
   }
 }
